3f02cb782cb1307695b9a223c879d1d8e65c392f
Tadashi Junior Suto <xtadashix@gmail.com>
2026-02-16 18:44:50 -0300

    Create skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,32 @@
+export const skills = [
+  { 
+    name: 'PRODUCT OWNER', 
+    level: 95, 
+    type: 'legendary',
+    description: 'Priorização, visão de produto, valor de negócio'
+  },
+  { 
+    name: 'GESTÃO DE PROJETOS', 
+    level: 92, 
+    type: 'legendary',
+    description: 'Sprints, prazos, recursos, riscos'
+  },
+  { 
+    name: 'INTEGRAÇÕES', 
+    level: 88, 
+    type: 'epic',
+    description: 'APIs, web services, aéreas, hotéis'
+  },
+  { 
+    name: 'UX', 
+    level: 82, 
+    type: 'rare',
+    description: 'Pesquisa, protótipos, jornada do usuário'
+  },
+  { 
+    name: 'LIDERANÇA', 
+    level: 90, 
+    type: 'epic',
+    description: 'Gestão de equipes multifuncionais'
+  }
+];
